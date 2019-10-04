50f84973aaa7c8c04ea06e310503516a8096b8ba
dgnguyen <ducgiang1206@gmail.com>
2019-10-04 22:54:27 +0200

    fix error backend to deploy

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   // express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path')
-  appget('*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
