455218b9b5dab3621042c0069576d308ea2d2667
dgnguyen <ducgiang1206@gmail.com>
2020-02-07 07:52:23 +0100

    176 : webhook

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,21 @@
-import 'materialize-css/dist/css/materialize.min.css'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import reduxThunk from 'redux-thunk'
-//Developpement only axios helpers 142 - testing email sending
-// import axios from 'axios'
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
+import App from './components/App';
+import reducers from './reducers';
 
-import { createStore, applyMiddleware } from 'redux'
-import reducers from './reducers'
-import App from './components/App'
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
 
-//Developpement only axios helpers 142 - testing email sending
-// window.axios = axios
-
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
-  document.querySelector('#root'))
+  document.querySelector('#root')
+);
+
