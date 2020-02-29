0b70f6c71a3dfcb2f6a5f884b49a091452eb10ed
dgnguyen <ducgiang1206@gmail.com>
2020-02-29 23:01:49 +0100

    197 : use nrok instead of localtunel, update db from webhook sent from sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,28 +1,62 @@
-const mongoose = require('mongoose')
-const requireLogin = require('../middlewares/requireLogin')
-//check if user log in
-const requireCredits = require('../middlewares/requireCredits')
-//check if user have enough credits
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
-const Mailer = require('../services/Mailer')
-
-const Survey = mongoose.model('surveys')
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thanks for voting!')
-  })
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for voting!');
+  });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body)
-    res.send({})
-  })
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+        console.log("ending Update one method!");
+      })
+      .value();
+
+    res.send({});
+  });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-    //Save survey in mongoose
-    const { body, title, subject, recipients } = req.body
+    const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
@@ -30,24 +64,21 @@ module.exports = app => {
       body,
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
-      dateSend: Date.now(),
-    })
+      dateSent: Date.now()
+    });
 
-    // Prepare template and send survey to sendgrid
-    const mailer = new Mailer(survey, surveyTemplate(survey))
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
     try {
-      await mailer.send()
-      //save in database after send mail
-      await survey.save()
-      // credited user
-      req.user.credits -= 1
-      const user = await req.user.save()
-      //sendback user with credit updated
-      res.send(user)
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
     }
-    //Send back error if something wrong
-    catch (err) {
-      res.status(422)
-    }
-  })
-}
+  });
+};
